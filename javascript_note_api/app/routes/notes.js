1aebf38c2aacd60cbb3dc39eb9175b59318f6ca5
AllesonCoelho <alleson_henrique@hotmail.com>
2020-12-17 23:26:39 -0300

    Atualizando Notas

--- javascript_note_api/app/routes/notes.js
+++ javascript_note_api/app/routes/notes.js
@@ -46,6 +46,30 @@ router.get('/', withAuth, async(req, res) => {
     }
 })
 
+router.put('/:id', withAuth, async(req, res)=> {
+    const {title, body} = req.body
+    const {id} = req.params
+    try {
+        let note = await Note.findById(id)
+        if(isOwner(req.user, note)){
+            let note = await Note.findOneAndUpdate(
+                {_id:id}, 
+                {$set:{ title: title, body : body }},
+                {upsert: true, 'new':true}
+
+                )
+
+                res.json(note)
+
+        }else
+            res.status(403).json({error: 'Permission Denied'})
+        
+    } catch (error) {
+        res.status(500).json({error: 'Problem to update a Note'})
+        
+    }
+})
+
 const isOwner = (user, note) =>{
     if(JSON.stringify(user._id) == JSON.stringify(note.author._id))
         return true
